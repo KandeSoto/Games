a62a22d1791081c5ca66287636b355f66da30a4e
KandeSoto <kandelariosoto@gmail.com>
2020-07-07 12:05:02 -0700

    Ligeros cambios de diseño

--- Game.Data/Models/Country.cs
+++ Game.Data/Models/Country.cs
@@ -9,7 +9,8 @@ namespace Game.Data.Models
         public int CountryId { get; set; }
         [Required]
         public string Name { get; set; }
-        public bool? IsActive { get; set; }
+        public bool IsActive { get; set; }
+        public string Active => IsActive ? "Active" : "Inactive";
         public DateTime Registration { get; set; }
         public DateTime? Modification { get; set; }
     }
